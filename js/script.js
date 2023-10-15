d43e7578d78d1fb5d045fe5964c98689826cdd08
carldamey <carldamey@gmail.com>
2023-10-15 15:19:05 -0700

    solves tasks 1.0 - 1.3 and defers script in index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Task 1.0
+const mainEl = document.querySelector("main")
+
+// Task 1.1
+mainEl.style.backgroundColor = "var(--main-bg)"
+
+// Task 1.2
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>"
+
+// Task 1.3
+mainEl.classList.add("flex-ctr")
